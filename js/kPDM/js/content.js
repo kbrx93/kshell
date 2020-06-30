63438904a85efb8b9fff78b8c48bf842eff98fac
kbrx93 <kbrx93@gmail.com>
2020-07-01 02:31:26 +0800

    update kPDM

--- js/kPDM/js/content.js
+++ js/kPDM/js/content.js
@@ -2,6 +2,9 @@ videojs.options.flash.swf = "video-js.swf"
 const video_support_format = ['.mp4', '.webm', '.ogv']
 const log_support_format = ['.zip', 'rar']
 let count = 1
+let logcount = 1
+let file_path = 'K://log/'
+let bugNum = 'BBB'
 // 查找所有视频及Log
 $("a[href][target='_blank']").each(function () {
   let $this = $(this)
@@ -52,8 +55,22 @@ $("a[href][target='_blank']").each(function () {
     })
   } else if (log_support_format.indexOf(suffix) >= 0) {
     // 匹配对应的 Log
-    let $play = $('<a class="video-play-btn" href="javascript:;">播放</a>')
-    $this.after($play)
+    let $btn = $('<button id="btn">点我复制LOG路径</button>')
+    $this.after($btn)
+    $btn.click(function() {
+      // 后面改成可配置的
+      let fileurl = file_path + bugNum + '_' + logcount + "/"
+      logcount ++
+      const input = document.createElement('input');
+      document.body.appendChild(input);
+      input.setAttribute('value', fileurl);
+      input.select();
+      if (document.execCommand('copy')) {
+        document.execCommand('copy');
+        console.log('复制成功');
+      }
+      document.body.removeChild(input);
+    })
   } else {
     console.log('can not find support suffix log and video')
   }
