cf04cf5605ce0a6005e96efe886a2da9d21fd09b
kbrx93 <kbrx93@gmail.com>
2021-12-02 23:17:50 +0800

    Update vps_init.sh

--- bash/vps_init.sh
+++ bash/vps_init.sh
@@ -322,7 +322,7 @@ update_ssh_port()
 {
   if [ $1 == 'y' ]; then
     # bash <(wget -N --no-check-certificate -qO- https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ssh_port.sh)
-    apt update -y && apt install -y curl && bash <(curl -fsSL https://zfile.kbrx93.com/directlink/1/Public/key.sh) -og $2 -p $3 -d
+    apt update -y && apt install -y curl && bash <(curl -fsSL https://file.kbrx93.com/directlink/1/public/share/key.sh) -og $2 -p $3 -d
   fi
 }
 
